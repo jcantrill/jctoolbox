c3a99ea44ac60305305426da4f1ce4611dbc399f
Jeff Cantrill <jcantril@redhat.com>
2016-10-19 09:23:35 -0400

    1.3 updates

--- openshift/startOpenShiftOnDocker.sh
+++ openshift/startOpenShiftOnDocker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#
+# Needed before hand?: sudo chcon -R -t svirt_sandbox_file_t /tmp/volumes
+#
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -18,7 +21,7 @@ source "${ORIGIN_DIR}/hack/util.sh"
 #not sure this works when there are other containers
 DEFAULT_SERVER_IP=`ifconfig | grep -Ev "(127.0.0.1|172.17.42.1)" | grep "inet " | head -n 1 | sed 's/adr://' | awk '{print $2}'`
 
-CONTAINER_NAME=openshift-origin
+CONTAINER_NAME=openshift
 DOCKER_EXEC="docker exec $CONTAINER_NAME"
 
 CONFIG_DIR=/var/lib/openshift/openshift.local.config
@@ -50,24 +53,29 @@ function osc_create_files_in(){
 }
 
 #if [[ "$SKIP_RESTART" -ne "false" ]]; then
-  RUNNING_ID=$(CONTAINER_ID=`docker ps -a | grep $CONTAINER_NAME`;echo ${CONTAINER_ID%%openshift*})
-  if [[ ! -z $RUNNING_ID ]]; then
-    echo "Found an instance already running.  Removing it."
-    docker rm -f $RUNNING_ID
+  if [[ ! -z $(docker ps -a | grep $CONTAINER_NAME) ]]; then
+    echo "Found an existing container.  Removing it."
+    docker rm -f $CONTAINER_NAME
   fi
 
   if [[ ! -e $HOST_DATA_VOLUME ]]; then
     mkdir $HOST_DATA_VOLUME
   fi
 
-  MOUNT_VOLUMES="-v $HOST_DATA_VOLUME:/tmp/openshift -v $ORIGIN_DIR:/tmp/data"
+  MOUNT_VOLUMES="-v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys:ro -v /var/lib/docker:/var/lib/docker:rw -v /var/lib/openshift/openshift.local.volumes:/var/lib/openshift/openshift.local.volumes"
+  #MOUNT_VOLUMES="$MOUNT_VOLUMES -v $HOST_DATA_VOLUME:/tmp/openshift -v $ORIGIN_DIR:/tmp/origin -v /tmp/volumes:/tmp/volumes"
+  MOUNT_VOLUMES="$MOUNT_VOLUMES -v /var/run/docker.sock:/var/run/docker.sock -v $HOST_DATA_VOLUME:/tmp/openshift"
+  MOUNT_VOLUMES="$MOUNT_VOLUMES -v $ORIGIN_DIR:/tmp/origin"
   if [ -d $JBOSS_TEMPLATE_DIR ]; then
     MOUNT_VOLUMES="$MOUNT_VOLUMES -v $JBOSS_TEMPLATE_DIR:/tmp/jboss"
   fi
 
 
   echo "Starting OpenShift Server..."
-  CONTAINER_ID=$(docker run -d --name "$CONTAINER_NAME" --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock $MOUNT_VOLUMES openshift/origin:$IMAGE_TAG start)
+  RUN_OPTS="--name $CONTAINER_NAME --net=host --privileged"
+  OS_OPTS="--volume-dir=/tmp/openshift"
+  #CONTAINER_ID=$(docker run -d $RUN_OPTS $MOUNT_VOLUMES openshift/origin:$IMAGE_TAG start --volume-dir=/tmp --master='https://localhost:8443')
+  CONTAINER_ID=$(docker run -d $RUN_OPTS $MOUNT_VOLUMES openshift/origin:$IMAGE_TAG start $OS_OPTS)
 
   echo "Waiting for the server to become available..."
   wait_for_command "docker logs $CONTAINER_ID 2>&1 | grep \"Starting kubelet main sync loop\"" $((45*TIME_SEC))
@@ -80,13 +88,13 @@ $DOCKER_EXEC osadm policy add-cluster-role-to-user $ADMIN_ROLE $ADMIN_USER --con
 
 #deploy registry
 echo "Deploying the OpenShift Docker registry..."
-$DOCKER_EXEC osadm registry --credentials=$CONFIG_DIR/master/openshift-registry.kubeconfig --config=$ADMIN_CREDENTIALS
+$DOCKER_EXEC osadm registry --credentials=$CONFIG_DIR/master/openshift-registry.kubeconfig --config=$ADMIN_CREDENTIALS --mount-host=$HOST_DATA_VOLUME
 
 #initialize server content
 echo "Adding image streams..."
-osc_create /tmp/data/examples/image-streams/image-streams-centos7.json
+osc_create /tmp/origin/examples/image-streams/image-streams-centos7.json
 echo "Adding templates..."
-osc_create /tmp/data/examples/sample-app/application-template-stibuild.json
+osc_create /tmp/origin/examples/sample-app/application-template-stibuild.json
 
 if [ -d $JBOSS_TEMPLATE_DIR ]; then
   set +e
