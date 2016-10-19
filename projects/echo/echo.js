ab3a040401bff6d5563f8070c696d2bd12f46d83
Jeff Cantrill <jcantril@redhat.com>
2016-10-19 09:25:48 -0400

    init echo

---
+++ projects/echo/echo.js
@@ -0,0 +1,19 @@
+
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 9999);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/echo/:data', function(req, res) {
+  res.send(req.params.data);
+});
