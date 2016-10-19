31f6dfdd3055536d4285a60f0278bfb65034db00
Jeff Cantrill <jcantril@redhat.com>
2016-10-19 13:15:29 -0400

    add log message

--- projects/logtest/app/index.js
+++ projects/logtest/app/index.js
@@ -13,17 +13,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req,res, next){
+    console.log('hitting root CONTEXT.. rendering inputs');
     res.render('inputs');
 });
 
 app.post('/running', upload.array(), function(req, res, next){
+    console.log('hitting running CONTEXT.. rendering running');
     var formData = req.body;
     console.log(util.format('Starting with params: %j', formData));
     res.render('running');
 });
 
 app.get('/hello', function(req, res, next){
+    console.log('hitting hello CONTEXT.. rendering hello');
     res.render('hello');
+
 });
 
 app.listen(3000, function(){
