3c7e6d7d318fbc248a848c13894e7622d4065a58
Jeff Cantrill <jcantril@redhat.com>
2016-10-19 12:00:18 -0400

    init running page

--- projects/logtest/app/index.js
+++ projects/logtest/app/index.js
@@ -1,8 +1,27 @@
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var util = require('util');
 var express = require('express');
-var app = express();
 
-app.get('/', function(req,res){
-    res.send('hello world');
+var app = express();
+var upload = multer();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req,res, next){
+    res.render('inputs');
+});
+
+app.post('/running', upload.array(), function(req, res, next){
+    var formData = req.body;
+    console.log(util.format('Starting with params: %j', formData));
+    res.render('running');
+});
+
+app.get('/hello', function(req, res, next){
+    res.render('hello');
 });
 
 app.listen(3000, function(){
